f2f40de5c81a9c58a6e6e2ceb8f7271964ce40ab
phealy1982 <phealy1982@gmail.com>
2017-07-02 14:29:33 +0100

    updates

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function onLoad(){
     var xhr = new XMLHttpRequest();
+    var response;
     xhr.open("GET", "https://pool-league-api.herokuapp.com/api/player", true);
     xhr.onload = function (e) {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
           response = JSON.parse(xhr.responseText);
+          showPlayers(response);
         } else {
           console.error(xhr.statusText);
         }
@@ -13,5 +15,16 @@ function onLoad(){
     xhr.onerror = function (e) {
       console.error(xhr.statusText);
     };
-    
+}
+
+function showPlayers(players){
+    var htmlText = '';
+
+                for (var player in players) {
+                    htmlText += '<div class="div-conatiner">';
+                    htmlText += '<p class="p-name"> Name: ' + player.name + '</p>';
+                    htmlText += '</div>';
+                }
+
+                document.getElementsByTagName("body").append(htmlText);
 }
